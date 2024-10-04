3de474ebc5f0c412fdaee9cfa38eda73c58d3895
Yaroslav Trofimov <159535808+CoR0NaExTrA@users.noreply.github.com>
2024-10-04 16:52:14 +0300

    lw1

---
+++ Task12/Task12/Task12.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+
+bool isStartOfSentence(const std::string& word, const std::string& prevWord) {
+    return (prevWord.empty() || prevWord.back() == '.' || prevWord.back() == '!' || prevWord.back() == '?');
+}
+
+bool isTargetWord(const std::string& word) {
+    return (word == "который" || word == "которая" || word == "которое" || word == "которые");
+}
+
+int main() {
+    
+}
+
